db9b8989a9cc976d35a7a208bfeb521eb6de050a
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-10-27 22:00:45 +0300

    backend<>adding an api that gets all the categories

--- backend/app/Http/Controllers/CategoryController.php
+++ backend/app/Http/Controllers/CategoryController.php
@@ -1,14 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     function getCategories(Request $request)
     {
-      //
+        $category = Category::select("name")->get();
+
+        return response()->json([
+            "status"=>"success",
+            "data"=>$category,
+        ]);
     }
- 
 }
