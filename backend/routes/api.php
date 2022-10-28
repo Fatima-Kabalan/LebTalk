24a9157311f2c4d27c32c21a6794065fc2d904f2
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-10-28 21:00:28 +0300

    backend<>routing the api that deletes a question

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -23,6 +23,7 @@ Route::group(["prefix"=> "v1"], function(){
         Route::post("/deleteQuiz", [QuizController::class, "deleteQuiz"])->name("deleteQuiz"); 
         Route::get("/getQuestion", [QuestionController::class, "getQuestion"])->name("getQuestion"); 
         Route::post("/addQuestion", [QuestionController::class, "addQuestion"])->name("addQuestion"); 
+        Route::post("/deleteQuestion", [QuestionController::class, "deleteQuestion"])->name("deleteQuestion"); 
 
         Route::get("/getCards", [CardController::class, "getCards"])->name("getCards"); 
         Route::post("/addCards", [CardController::class, "addCards"])->name("addCards"); 
