d22c024c3da162b6a804f73ce8c4c1625d025713
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-10-28 14:05:07 +0300

    backend<>creating seeders

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -11,5 +11,69 @@ use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
-   
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'fatima1',
+            'email' => 'fatima22@fatima.com',
+            'password' => '1234567',
+            'phone_number' => '70456311',
+            'user_roles_id' => '1'
+        ]);
+        User::create([
+            'name' => 'Ahmad2',
+            'email' => 'ahmad22@ahmad.com',
+            'password' => '12345678',
+            'phone_number' => '70987123',
+            'user_roles_id' => '1'
+        ]);
+        User::create([
+            'name' => 'karem2',
+            'email' => 'karem@karem42.com',
+            'password' => '123456789',
+            'phone_number' => '70987654',
+            'user_roles_id' => '3'
+        ]);
+        User::create([
+            'name' => 'nour2',
+            'email' => 'nour@nour23.com',
+            'password' => '123456898',
+            'phone_number' => '70123654',
+            'user_roles_id' => '2'
+        ]);
+        User::create([
+            'name' => 'souraya1',
+            'email' => 'souraya@souraya23.com',
+            'password' => '9876543',
+            'phone_number' => '70123554',
+            'user_roles_id' => '3'
+        ]);
+       
+        Card::create([
+            'card_image' => 'jpg1',
+            'voice_note' => 'mp32',
+            'english_text' => 'hi',
+            'arabic_text' => 'هلا',
+            'categories_id' => '1'
+        ]);
+
+        Card::create([
+            'card_image' => 'png',
+            'voice_note' => 'mp3',
+            'english_text' => 'bye',
+            'arabic_text' => 'باي',
+            'categories_id' => '2'
+        ]);
+
+        Category::create([
+            'name' => 'food1',
+            'admin_id' => '1',
+        ]);
+
+        Category::create([
+            'name' => 'sport',
+            'admin_id' => '2',
+        ]);
+    }
 }
