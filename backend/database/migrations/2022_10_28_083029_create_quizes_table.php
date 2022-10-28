cc07745a4deb9cd9de26e2edae1009a29b5c0ab1
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-10-28 11:37:41 +0300

    backend<>creating quizes table

---
+++ backend/database/migrations/2022_10_28_083029_create_quizes_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('quizes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('question');
+            $table->string('status');
+            $table->time('time_out');
+            $table->time('time_delivered');
+            $table->string('users_id');
+            $table->string('categories_id');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('quizes');
+    }
+};
