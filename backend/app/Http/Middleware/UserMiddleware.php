71d8db5f6bfa428cca748f60d1d356f188f8d225
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-10-31 16:09:48 +0200

    backend<>adding user middleware

--- backend/app/Http/Middleware/UserMiddleware.php
+++ backend/app/Http/Middleware/UserMiddleware.php
@@ -4,14 +4,13 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Auth;
 
-class AdminMiddleware
+class UserMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
         if($user = Auth::user()){
-            if($user->user_roles_id == 1){
+            if($user->user_roles_id == 3){
             return $next($request);
         }
             return response()->json([
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,26 +15,23 @@ Route::post("/register", [AuthController::class, "register"])->name("register");
 Route::group(["prefix"=> "v1"], function(){
 
     Route::group(["middleware" => ["admin.type"]], function(){
-Route::post("/addCategory", [CategoryController::class, "addCategory"])->name("addCategory"); 
-    });
-        Route::get("/getCategory/{id}", [CategoryController::class, "getCategory"])->name("getCategory"); 
-        
+        Route::post("/addCategory", [CategoryController::class, "addCategory"])->name("addCategory"); 
         Route::post("/deleteCategory", [CategoryController::class, "deleteCategory"])->name("deleteCategory"); 
-        Route::get("/getAllCategories", [CategoryController::class, "getALlCategories"])->name("getALlCategories"); 
-
         Route::post("/addQuiz", [QuizController::class, "addQuiz"])->name("addQuiz"); 
-        Route::get("/getQuiz", [QuizController::class, "getQuiz"])->name("getQuiz"); 
         Route::post("/deleteQuiz", [QuizController::class, "deleteQuiz"])->name("deleteQuiz"); 
-
-        Route::get("/getQuestion", [QuestionController::class, "getQuestion"])->name("getQuestion"); 
         Route::post("/addQuestion", [QuestionController::class, "addQuestion"])->name("addQuestion"); 
         Route::post("/deleteQuestion", [QuestionController::class, "deleteQuestion"])->name("deleteQuestion"); 
-
-        Route::get("/getCards", [CardController::class, "getCards"])->name("getCards"); 
         Route::post("/addCard", [CardController::class, "addCard"])->name("addCard"); 
         Route::post("/deleteCard", [CardController::class, "deleteCard"])->name("deleteCard"); 
-
+    }); 
+        Route::get("/getCategory/{id}", [CategoryController::class, "getCategory"])->name("getCategory");  
+        Route::get("/getAllCategories", [CategoryController::class, "getALlCategories"])->name("getALlCategories"); 
+        Route::get("/getQuiz", [QuizController::class, "getQuiz"])->name("getQuiz"); 
+        Route::get("/getQuestion", [QuestionController::class, "getQuestion"])->name("getQuestion"); 
+        Route::get("/getCards", [CardController::class, "getCards"])->name("getCards"); 
+       
+    Route::group(["middleware" => ["user.type"]], function(){
         Route::post("/favCard", [CardController::class, "favCard"])->name("favCard"); 
         Route::post("/unFavCard", [CardController::class, "unFavCard"])->name("unFavCard"); 
-    // });
+    });
 });
