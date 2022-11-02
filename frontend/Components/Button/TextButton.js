27791d2ae90bc471db30ba6a75fe1c4bc273ee7a
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-02 14:20:39 +0200

    frontend<>fixing text button component

--- frontend/Components/Button/TextButton.js
+++ frontend/Components/Button/TextButton.js
@@ -1,14 +1,25 @@
-import { Text, TouchableOpacity } from "react-native";
+import { StyleSheet, View ,TouchableOpacity ,Text} from 'react-native';
 
-
-export default function TextButton({onPress, title, styles}){
-    return (
-        <TouchableOpacity 
-            onPress={onPress}
-        >
-            <Text style={styles.redText}>
-              {title}
-            </Text>
-        </TouchableOpacity>
-    )
+export default function TextButton({text, style ,onPress}) {
+  return (
+    <View >
+      <TouchableOpacity onPress={onPress} >
+        <Text style={style}>{text}</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+    button:{
+      backgroundColor:'#F29D38',
+      borderRadius:30,
+      marginBottom:20,
+    },
+    btnText:{
+      color:'white',
+      textAlign:'center',
+      padding:6,
+      fontSize:18,
+      fontWeight:'bold',
+    },
+});
