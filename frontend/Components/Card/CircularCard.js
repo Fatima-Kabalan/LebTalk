8765c5e9bc67209c3af0a062f1944d87a5044d65
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-03 10:25:58 +0200

    frontend<>fixing circular card component

--- frontend/Components/Card/CircularCard.js
+++ frontend/Components/Card/CircularCard.js
@@ -1,19 +1,21 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View ,Image} from 'react-native';
 
-export default function CircularCard({}) {
+export default function CircularCard({source}) {
   return (
-    <View style={styles.category}>
-      <Image
-        style={styles}
-        source={require('../../assets/tarboush.jpg')}
+    <View >
+      <Image style={styles.category}
+        source={source}
       />
     </View>
   );
 }
 const styles = StyleSheet.create({
    category:{
-        borderRadius:50,
+        borderRadius:70,
         backgroundColor:'white',
+        height:'45%',
+        width:'40%',
+        opacity:0.6,
    }
 });
 
