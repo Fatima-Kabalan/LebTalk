1933e3dfaf38da8a162522ca13df629277f4667a
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-03 16:09:11 +0200

    frontend<>fixing home page

--- frontend/Pages/HomePage.js
+++ frontend/Pages/HomePage.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View ,Text ,Image ,ImageBackground} from 'react-native';
+import { StyleSheet, View ,Text ,Image ,ImageBackground ,ScrollView} from 'react-native';
 import { useFonts } from 'expo-font';
 import * as Font from 'expo-font';
 import Logo from '../Components/Logo/SmallLogo';
@@ -7,20 +7,65 @@ import Card from '../Components/Card/CircularCard';
 export default function HomePage() {
   return (
         <View style={styles.container}>
-            <ImageBackground  source={require('../assets/lebanon.jpg')} style={styles.img}>
-            <View styles={styles.logoBar}> 
-                  <Logo/>
+            <View style={styles.top}>
+                <ImageBackground  source={require('../assets/lebanon.jpg')} style={styles.img}>
+                <View styles={styles.logoBar}> 
+                    <Logo/>
+                </View>
+                <View style={styles.textContainer}>
+                    <Text style={styles.imgText}> “In learning you will teach, and in teaching you will learn.”
+                    ― Phil Collins</Text>
+                </View>
+                </ImageBackground>
             </View>
-            <View style={styles.textContainer}><Text style={styles.imgText}> “In learning you will teach, and in teaching you will learn.”
-                ― Phil Collins</Text>
+            <ScrollView >
+                <Text>Courses</Text>
+                <View style={styles.buttom}>
+                <View style={styles.cardFlex}>  
+                    <Card source={require('../assets/lebanon.jpg')} text={'hi'}/>
+                    <Card source={require('../assets/lebanon.jpg')} />
+                </View>
+                <View style={styles.cardFlex}>
+                        <Card source={require('../assets/lebanon.jpg')}  />
+                        <Card source={require('../assets/lebanon.jpg')} />
+                </View>
             </View>
-            </ImageBackground>
-            <Card source={require('../assets/lebanon.jpg')}/>
+            </ScrollView>
+            
+          
         </View>
   );
 }
 
 const styles = StyleSheet.create({
+    top:{
+        height:'40%',
+        width:'100%',
+
+    },
+    cardFlex:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        padding:30,
+        marginTop:60
+    },
+    buttom:{
+        height:'60%',
+        width:'100%',
+        flexDirection:'column',
+        borderTopLeftRadius:50,
+        borderTopRightRadius:50,
+        alignItems:'center',
+        justifyContent:'space-evenly',
+       
+    },
+    cards:{
+        marginButtom:70,
+        paddingTop:20,
+        backgroundColor:'green'
+        
+    },
     container: {
         flex: 1,
         flexDirection:'column',
@@ -28,15 +73,15 @@ const styles = StyleSheet.create({
     },
     img:{
         width: '100%',
-        height: '60%',
+        height: '100%',
         opacity:0.9,
     },
     textContainer:{
         position: 'absolute',
-        top: 0, 
+        top: 60, 
         left: 0, 
         right: 0,
-        bottom: 99, 
+        bottom: 1, 
         justifyContent: 'center',
         alignItems: 'center',
     },
@@ -60,7 +105,9 @@ const styles = StyleSheet.create({
         color:'#E1943C',
     },
     logoBar:{
-        flexDirection:'column'
+        flexDirection:'row',
+        justifyContent:'space-around',
+        alignItems:'center',
     },
     boxContainer:{
         height:'40%',
