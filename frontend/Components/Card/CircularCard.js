e52695e6bb60702bf8c1f44aa3028e6d0af6ebde
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-03 16:08:07 +0200

    frontend<>styling the circular card component

--- frontend/Components/Card/CircularCard.js
+++ frontend/Components/Card/CircularCard.js
@@ -1,21 +1,30 @@
-import { StyleSheet, View ,Image} from 'react-native';
+import { StyleSheet, View ,Image, ImageBackground} from 'react-native';
+import TextButton from '../Button/TextButton';
 
-export default function CircularCard({source}) {
+export default function CircularCard({source,text}) {
   return (
-    <View >
-      <Image style={styles.category}
-        source={source}
-      />
+    <View>
+      <ImageBackground style={styles.category}  imageStyle={{ borderRadius: 80}}
+        source={source}>
+         <TextButton text={text} style={styles.text}></TextButton>
+      </ImageBackground>
     </View>
   );
 }
 const styles = StyleSheet.create({
+    text:{
+        color:'black',
+        fontSize:40,
+        padding:40,
+        textAlign:'center',
+        
+    },
    category:{
-        borderRadius:70,
         backgroundColor:'white',
-        height:'45%',
-        width:'40%',
+        height:150,
+        width:150,
         opacity:0.6,
+        margin:20,
    }
 });
 
