d14959bf5c0897a06b626923b27a21b9c996b29e
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-04 09:23:17 +0200

    frontend<>fixing the circular card component

--- frontend/Components/Card/CircularCard.js
+++ frontend/Components/Card/CircularCard.js
@@ -4,9 +4,10 @@ import TextButton from '../Button/TextButton';
 export default function CircularCard({source,text,onPress,textStyle}) {
   return (
     <View>
-      <ImageBackground style={styles.category}  imageStyle={{ borderRadius: 80}}
+      <ImageBackground style={styles.category}  
+    //   imageStyle={{ borderRadius: 80}}
         source={source}>
-            <TextButton text={text} onPress={onPress} style={styles.text}></TextButton>
+            <TextButton text={text} onPress={onPress} style={textStyle}></TextButton>
       </ImageBackground>
     </View>
   );
@@ -14,11 +15,11 @@ export default function CircularCard({source,text,onPress,textStyle}) {
 const styles = StyleSheet.create({
    category:{
         backgroundColor:'black',
-        borderRadius:80,
         height:150,
         width:150,
         opacity:0.7,
         margin:20,
+        borderRadius:80,
    },
    text:{
     textAlign:'center',
