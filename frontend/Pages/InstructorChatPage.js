7529f90ea6e56e24337ec4a50f64e11a4dff80ef
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-06 18:53:23 +0200

    frontend<>adding a page that shows all the instructors

--- frontend/Pages/InstructorChatPage.js
+++ frontend/Pages/InstructorChatPage.js
@@ -1,9 +1,19 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-
+import ChatCard from '../Components/Card/ChatCard';
 
 export default function InstructorChatPage(){
   return (
-    <View >
-        <Text>HI</Text>
+    <View style={styles.container}>
+       <ChatCard />
+
     </View>
-  );}
+  );
+}
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    flexDirection:'column',
+    justifyContent:'center',
+    alignItems:'center'
+  }
+})
