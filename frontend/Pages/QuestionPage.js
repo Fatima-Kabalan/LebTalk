0e8ee2348b554cd77d96df7b36dbdac59ad35972
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-07 21:24:59 +0200

    frontend<>adding buttons and text to the question page

--- frontend/Pages/QuestionPage.js
+++ frontend/Pages/QuestionPage.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View, Image , TouchableOpacity } from 'react-native';
 import { AntDesign } from '@expo/vector-icons'; 
 import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 import Logo from '../Components/Logo/SmallLogo';
+import ContainedButton from '../Components/Button/ContainedButton';
 
 
 export default function QuestionPage({navigation , source }) {
@@ -14,15 +15,18 @@ export default function QuestionPage({navigation , source }) {
             <MaterialCommunityIcons name="alarm-multiple" size={24} color="black" />
             <Logo/>
         </View>
-        <View style={styles.imgContainer}>
-            <Image source={require('../assets/Kibbeh.jpg')} style={styles.image}></Image>
-        </View>
-        <View >
+        <Image source={require('../assets/Kibbeh.jpg')} style={styles.image}>
+        </Image>
+        <View>
             <Text style={styles.questionNum}>Question1</Text>
+            <Text style={styles.question}>What is the name of the above meal?</Text>
         </View>
-            
+        <View style={styles.btnFlex}>
+            <ContainedButton buttonStyle={styles.btnSuccess} text={'كبة'} textStyle={styles.text}/>
+            <ContainedButton buttonStyle={styles.btnFail} text={'فتوش'} textStyle={styles.text2}/>
+            <ContainedButton buttonStyle={styles.btnFail} text={'حمص'} textStyle={styles.text2}/>
+        </View>
     </View>
-    
   );
 }
 const styles = StyleSheet.create({
@@ -33,26 +37,53 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         justifyContent:'space-between',
         padding:20,
-        marginTop:25
+        marginTop:25,
     },
     image:{
-        height:'55%',
+        height:'30%',
         width:'90%', 
-        borderRadius:30
-    },
-    imgContainer:{
-        alignItems:'center',
-        
+        borderRadius:30,
+        marginLeft:10,
+        marginRight:10
     },
     questionNum:{
         color:'#BB271A',
         fontWeight:'bold',
         fontSize:18,
-      
+        marginTop:20,
+        marginRight:20,
+        marginLeft:20,
     },
-    questionText:{
+    question:{
+        fontSize:16,
+        marginRight:20,
+        marginTop:20,
+        marginLeft:20,
+    },
+    btnSuccess:{
+        backgroundColor:'#F29D38',
+        width:'90%',
+        marginLeft:20,
+        marginTop:20,
+        borderRadius:40
+    },
+    btnFail:{
+        backgroundColor:'#d3d3d3',
+        width:'90%',
+        marginLeft:20,
+        marginTop:20,
+        borderRadius:40
+    },
+    text:{
+        fontSize:35,
         color:'#BB271A',
-        fontSize:14,
-        fontWeight:'normal'
+        textAlign:'center',
+        padding:10
     },
+    text2:{
+        fontSize:35,
+        color:'#778899',
+        textAlign:'center',
+        padding:10
+    }
 })
