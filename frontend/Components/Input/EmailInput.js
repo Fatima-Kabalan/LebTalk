c5d4652927f5b63a6cc47b1c7dc11bc07b2eea65
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-13 12:06:14 +0200

    frontend<>adding the correct value and setting the correct syntax for changing the input value of email at email input component

--- frontend/Components/Input/EmailInput.js
+++ frontend/Components/Input/EmailInput.js
@@ -2,11 +2,12 @@ import { StyleSheet, View , TextInput} from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
-export default function EmailInput() {
-  return (
+export default function EmailInput({email , setEmail}) {
+
+    return (
         <View style={styles.inputIcon}>
             <MaterialCommunityIcons name="email" size={20} color="#BD4543" />
-            <TextInput style={styles.input} type='email' placeholder='Email' ></TextInput>
+            <TextInput style={styles.input} type='email' name='email' placeholder='Email' value={email} onChangeText={setEmail}/>
         </View>
   );
 }
