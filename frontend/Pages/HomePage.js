37a26e5134c0662b6bdb3bb3e76ec18818beaecc
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-16 19:24:52 +0200

    frontend<>replacing the scroll view component with flt list component

--- frontend/Pages/HomePage.js
+++ frontend/Pages/HomePage.js
@@ -1,9 +1,5 @@
-import { StyleSheet, View ,Text ,Image ,ImageBackground ,ScrollView} from 'react-native';
-import { useFonts } from 'expo-font';
-import * as Font from 'expo-font';
-import Logo from '../Components/Logo/SmallLogo';
+import { StyleSheet, View, Text, ImageBackground, FlatList } from 'react-native';
 import Card from '../Components/Card/CircularCard';
-import HeaderNav from '../Components/HeaderNav';
 import { useState, useEffect } from 'react';
 import { IMAGE_URL, SERVER_URL } from "../env";
 import axios from "axios";
@@ -11,53 +7,29 @@ import axios from "axios";
 export default function HomePage({navigation}) {
     
     const [categories, setCategories] = useState([])
+    const [isLoading, setLoading] = useState(true);
 
-    // let categories = [
-    //     {
-    //         category_image: require('../assets/Kibbeh.jpg'),
-    //         category_name: 'Food'
-    //     },
-    //     {
-    //         category_image: require('../assets/fam.jpg'),
-    //         category_name: 'Family'
-    //     },
-    //     {
-    //         category_image: require('../assets/dog.jpg'),
-    //         category_name: 'Animals'
-    //     },
-    //     {
-    //         category_image: require('../assets/sports.jpg'),
-    //         category_name: 'Sports'
-    //     },
-    // ]
+    const Item = ({ item }) => (
+        <Card source={ IMAGE_URL + item.category_image} text={item.category_name} textStyle={styles.cardText} onPress={()=>navigation.navigate(item.category_name)} />
+    );
 
-    // const getAllCategories = () => {
-    //     return(
-    //         axios({
-    //             method: "GET",
-    //             url: `${SERVER_URL}/api/v1/getAllCategories`,
-    //           }).then((res) => {
-    //             console.log(res.data);
-    //             setCategories(res.data);
-    //           })
-    //     )
-    // }
-
-    // useEffect(() => {
-    //     categories = getAllCategories();
-    //     console.log(categories)
-    // }, [])
+    const renderItem = ( {item} ) => { 
+        return (
+          <Item
+            item={item}
+          />
+        );
+    };
 
     useEffect(() => {
-        console.log(categories);
         axios({
           method: "GET",
           url: `${SERVER_URL}/api/v1/getAllCategories`,
-        }).then((res) => {
-          console.log(res.data);   
+        }).then((res) => { 
           setCategories(res.data.data);
-        });
-      }, [categories])
+        }).catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+      }, [])
 
     if (categories)
     return (
@@ -70,61 +42,27 @@ export default function HomePage({navigation}) {
                 </View>
                 </ImageBackground>
             </View>
-            <ScrollView>
-                <View >
-                    <View> 
-                    {categories?.map((category, index) => {
-                        console.log(categories)
-                        return(
-                            <Card source={ IMAGE_URL + category.category_image} text={category.category_name} textStyle={styles.cardText} onPress={()=>navigation.navigate(category.category_name)} />
-                        );
-                    })}
-                    </View>
-                </View>
-            </ScrollView>
-        </View>
+            
+            <View style={styles.list}>
+                <FlatList
+                    data={categories}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.id}
+                    numColumns={2}
+                />
+            </View>
+         </View>
   );
   return <></>
 }
 
 const styles = StyleSheet.create({
     top:{
-        height:'40%',
+        height:'50%',
         width:'100%',
     },
-    cardFlex:{
-        // flexWrap: 'wrap',
-        // flexDirection:'column',
-        justifyContent:'space-between',
-        alignItems:'center',
-        // padding:30,
-        // marginTop:10
-        flexDirection: "column",
-        flexWrap: "wrap",
-      
-    },
-    buttom:{
-        // height:'10%',
-        // width:'100%',
-        flexWrap: 'wrap',
-        flexDirection:'row',
-        // flexDirection:'column',
-        borderTopLeftRadius:50,
-        borderTopRightRadius:50,
-        alignItems:'center',
-        // justifyContent:'space-evenly',
-        // marginTop:30
-    },
-    cards:{
-        marginButtom:70,
-        paddingTop:20,  
-    },
-    container: {
-        flex: 1,
-        // flexDirection:'row',
-        // padding:10,
-        backgroundColor: 'white',
-        flexWrap: 'wrap',
+    list: {
+        justifyContent: 'space-between',
     },
     img:{
         width: '100%',
@@ -147,29 +85,6 @@ const styles = StyleSheet.create({
         opacity:7,
         backgroundColor:'#A42E2C',
         fontFamily: 'sans-serif',
-    },
-    lebText:{
-        fontWeight:'bold',
-        fontSize:30,
-        color:'white',
-       
-    },
-    talkText:{
-        fontWeight: 'bold',
-        fontSize:30,
-        color:'#E1943C',
-    },
-    logoBar:{
-        flexDirection:'row',
-        justifyContent:'space-around',
-        alignItems:'center',
-    },
-    courseText:{
-        paddingLeft:45,
-        textAlign:"left",
-        fontWeight:'bold',
-        fontSize:25,
-        paddingTop:10,
     },
     cardText:{
         color:'white',
