9730fb8d08a845e0f8fa715c3a84011ace7994cb
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-18 16:09:38 +0200

    frontend<>linking the instructors with whatsapp

--- frontend/Pages/InstructorChatPage.js
+++ frontend/Pages/InstructorChatPage.js
@@ -1,18 +1,40 @@
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View , Linking } from 'react-native';
 import ChatCard from '../Components/Card/ChatCard';
 import HeaderNav from '../Components/HeaderNav';
+import { useState,useEffect } from 'react';
+import axios from 'axios';
+import { IMAGE_URL, SERVER_URL } from "../env";
 
 export default function InstructorChatPage(){
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: `${SERVER_URL}/api/v1/getInstructors`,
+    }).then((res) => { 
+      setCards(res.data.data);
+    }).catch((error) => console.error(error))
+    .finally(() => setLoading(false));
+  }, [])
+
+console.log(cards);
+
+if (cards)
   return (
     <View style={styles.container}>
       <ScrollView style={styles.scroll}>
         <HeaderNav text={'Instructors'}/>
-        <ChatCard username={'Ahmad Kanso'} />
-        <ChatCard username={'Ahmad Kanso'} />
-        <ChatCard username={'Ahmad Kanso'} />
-        <ChatCard username={'Ahmad Kanso'} />
-        <ChatCard username={'Ahmad Kanso'} />
-        <ChatCard username={'Ahmad Kanso'} />
+        { cards?.map((card,index) =>{
+                    console.log(cards)
+                    return(
+                        <ChatCard source={ IMAGE_URL + card.profile_picture}  username={card.username} onPress={ ()=> Linking.openURL(
+                          'http://api.whatsapp.com/send?phone=961' + card.phone_number
+                        )}  
+                       
+                        />
+                    )
+                })}
       </ScrollView>
     </View>
   );
