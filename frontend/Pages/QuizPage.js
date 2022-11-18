3058eb8bc9f15d2c08458343932a3c9e31f9bb5c
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-18 16:14:21 +0200

    frontend<>adding questions to each quiz

--- frontend/Pages/QuizPage.js
+++ frontend/Pages/QuizPage.js
@@ -1,17 +1,46 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import ContainedButton from '../Components/Button/ContainedButton';
 import QuestionCard from '../Components/Card/QuestionCard';
 import HeaderNav from '../Components/HeaderNav';
+import { useState,useEffect } from 'react';
+import axios from 'axios';
+import { IMAGE_URL, SERVER_URL } from "../env";
 
-export default function QuizPage() {
+
+export default function QuizPage({navigation}) {
+
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: `${SERVER_URL}/api/v1/getQuiz`,
+    }).then((res) => { 
+      setCards(res.data.data);
+    }).catch((error) => console.error(error))
+    .finally(() => setLoading(false));
+  }, [])
+
+console.log(cards);
+
+if (cards)
   return (
     <View style={styles.container} >
-      <HeaderNav text={'Quiz'}/>
-      <View style={styles.questionContainer}>
-        <QuestionCard questionNum={'Question1'} question={'What is the name of this meal?'}/>
-        <QuestionCard questionNum={'Question1'} question={'What is the name of this meal?'}/>
-        <QuestionCard questionNum={'Question1'} question={'What is the name of this meal?'}/>
-      </View>
+      <HeaderNav text={'Quiz'} />
+
+        <ScrollView>
+        <View style={styles.questionContainer}>
+            { cards?.map((card,index) =>{
+                    console.log(cards)
+                    if(card.categories_id == 1)
+                    return(
+                        <QuestionCard onPress={() => navigation.navigate('Question')} source={ IMAGE_URL + card.card_image} quizNum={card.title} />
+                    )
+                })}
+                </View>
+        </ScrollView>
+       
+      
     </View>
   );
 }
