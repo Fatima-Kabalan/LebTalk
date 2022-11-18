9ba6e6217a862c67a4511a8ffbf706e3876cb81e
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-18 18:05:04 +0200

    backend<>adding role to the login at auth controller

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -1,27 +1,31 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use App\Models\UserRoles;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
 
-    public function __construct()
-    {
-        $this->middleware('auth:api', ['except' => ['login','register']]);
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth:api', ['except' => ['login','register']]);
+    // }
 
+
+    
     public function login(Request $request)
     {
         $request->validate([
             'email' => 'required|string|email',
             'password' => 'required|string',
         ]);
+        
         $credentials = $request->only('email', 'password');
 
         $token = Auth::attempt($credentials);
@@ -34,11 +38,11 @@ class AuthController extends Controller
         }
 
         $user = Auth::user();
-        $role = UserRoles::find(1);
+        // $role = UserRoles::find(1);
         return response()->json([ 
                 'status' => 'success',
                 'user' => $user,
-                'role' => $role->name,
+                // 'role' => $role->name,
                 'authorisation' => [
                     'token' => $token,
                     'type' => 'bearer',
@@ -48,18 +52,22 @@ class AuthController extends Controller
     }
 
     public function register(Request $request){
-        $request->validate([
+
+       $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
-        ]);
+            'phone_number'=>'required|string',
+        ]); 
+        
+        $user_roles_id = 2;
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'phone_number'=>$request->phone_number,
-            'user_roles_id'=>$request->user_roles_id,
+            'user_roles_id'=>$user_roles_id,
         ]);
 
         $token = Auth::login($user);
