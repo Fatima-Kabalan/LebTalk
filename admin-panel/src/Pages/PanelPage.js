a888d630df671a1f94a26c5b09e62e6574fc16da
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-18 18:01:43 +0200

    admin-panel<>adding components to panel page

--- admin-panel/src/Pages/PanelPage.js
+++ admin-panel/src/Pages/PanelPage.js
@@ -1,13 +1,29 @@
-import React from "react";
-import NavBar from "../Components/NavBar";
-import Table from "../Components/Table";
-const PanelPage = () => {
-    return(
-        <div className="main_container">
-           <NavBar/>
-           <Table/>
-        </div>
-    )
-}
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import NavBar from '../Components/NavBar';
+import SideBar from '../Components/SideBar';
+import Content from '../Components/Content';
 
-export default PanelPage;
+export default function PersistentDrawerLeft() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <NavBar open={open} handleDrawerOpen={handleDrawerOpen} />
+      <SideBar open={open} handleDrawerClose={handleDrawerClose} />
+      <Content open={open} />
+    </Box>
+  );
+}
