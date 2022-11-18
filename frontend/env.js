70d630cb64986643faaa2824b7514e2e988fa6c4
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-18 16:41:26 +0200

    frontend<>adding voice url to env

--- frontend/env.js
+++ frontend/env.js
@@ -1,2 +1,3 @@
-export const SERVER_URL = "http://192.168.16.111:5000";
-export const IMAGE_URL = "http://192.168.16.111:5000/storage/assets";
+export const SERVER_URL = "http://192.168.16.106:5000";
+export const IMAGE_URL = "http://192.168.16.106:5000/storage/assets";
+export const VOICE_URL = "http://192.168.16.106:5000/storage/assets/sounds";
