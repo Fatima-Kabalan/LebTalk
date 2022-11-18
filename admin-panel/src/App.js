58379efc6225501516de09f1a429a15a9d39fa14
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-18 18:02:30 +0200

    admin-panel<>adding routes

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import PanelPage from "./Pages/PanelPage";
+import {Route,BrowserRouter,Routes} from 'react-router-dom';
+import SideBar from './Components/SideBar';
+import user from './Pages/UserPage';
+import UserPage from './Pages/UserPage';
+
 
 function App() {
   return (
     <div className="App">
-      <PanelPage/>
+    <>
+    <BrowserRouter>
+      <PanelPage/> 
+          <Routes>
+              <Route path='/user'  element={<UserPage/>} />
+          </Routes>
+    </BrowserRouter>
+    </>
     </div>
   );
 }
