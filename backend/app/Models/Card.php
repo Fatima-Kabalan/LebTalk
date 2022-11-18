d91fa60d30aea38e151834dd8420b08304aae5c9
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-18 22:25:35 +0200

    backend<>adding relations to card model

--- backend/app/Models/Card.php
+++ backend/app/Models/Card.php
@@ -20,6 +20,11 @@ class Card extends Model
 
     function category(){
         return $this->belongsTo(Category::class);
-    }    
+    }  
+    
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
 
