9368ddc021e626fd86accff2c2c54191714ca864
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-20 18:49:10 +0200

    backend<>migrating favorites table

--- backend/database/migrations/2022_10_27_145849_create_favorites_table.php
+++ backend/database/migrations/2022_10_27_145849_create_favorites_table.php
@@ -10,9 +10,8 @@ return new class extends Migration
     {
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
-            $table->integer('users_id');
-            $table->integer('cards_id');
-
+            $table->integer('user_id')->unsigned();
+            $table->integer('card_id')->unsigned();
             $table->timestamps();
         });
     }
