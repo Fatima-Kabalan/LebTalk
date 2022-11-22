9f51f1315f64ee6645d096feb57ae37b32661629
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-22 15:09:24 +0200

    frontend<>adding props to contained button component

--- frontend/Components/Button/ContainedButton.js
+++ frontend/Components/Button/ContainedButton.js
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import { StyleSheet, View ,TouchableOpacity ,Text} from 'react-native';
 
-export default function ContainedButton({text,textStyle, buttonStyle ,onPress}) {
+export default function ContainedButton({text,textStyle, buttonStyle ,onPress , isCorrect}) {
+  const [pressed , setPressed] = useState(false);
   return (
     <View >
-      <TouchableOpacity onPress={onPress} style={buttonStyle}>
+      <TouchableOpacity  onPress={
+        ()=>{
+          setPressed(true);
+          onPress();
+        }
+      } style={pressed && isCorrect===0 ?  styles.btnWrong : pressed && isCorrect===1 ?  styles.btnCorrect : buttonStyle}>
         <Text style={textStyle}>{text}</Text>
       </TouchableOpacity>
     </View>
@@ -22,4 +29,19 @@ const styles = StyleSheet.create({
       fontSize:18,
       fontWeight:'bold',
     },
+
+    btnWrong:{
+      backgroundColor:'red',
+      width:'90%',
+      marginLeft:20,
+      marginTop:20,
+      borderRadius:40
+    },
+    btnCorrect:{
+      backgroundColor:'green',
+      width:'90%',
+      marginLeft:20,
+      marginTop:20,
+      borderRadius:40
+    }
 });
