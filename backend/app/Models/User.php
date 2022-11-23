59a220e949d29dde8304c494a8d7c608d5776b32
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-23 08:15:17 +0200

    backend<>adding id to user model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -13,6 +13,7 @@ class User extends Authenticatable implements JWTSubject
     use HasFactory, Notifiable;
 
     protected $fillable = [
+        'id',
         'name',
         'email',
         'password',
