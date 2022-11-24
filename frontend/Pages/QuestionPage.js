04866ea1c66961b2d233b328f4e34987fe7fd678
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-24 15:55:12 +0200

    frontend<>adding usestates for timer and styling the buttons

--- frontend/Pages/QuestionPage.js
+++ frontend/Pages/QuestionPage.js
@@ -16,7 +16,8 @@ import axios from "axios";
 import { IMAGE_URL, SERVER_URL } from "../env";
 import AnswersPage from "./AnswersPage";
 import { AntDesign } from "@expo/vector-icons";
-import { Audio } from 'expo-av';
+import { Audio } from "expo-av";
+import CountDown from "react-native-countdown-component";
 
 export default function QuestionPage({ route, navigation }) {
   const category_id = route.params.category_id;
@@ -24,21 +25,25 @@ export default function QuestionPage({ route, navigation }) {
 
   const [cards, setCards] = useState();
   const [answers, setAnswers] = useState([]);
-  const [lengthh , setlengthh] = useState(0);
+  const [lengthh, setlengthh] = useState(0);
+  const [answered, setAnswered] = useState(false);
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [currentId, setCurrentId] = useState(0);
-  const [appear, setAppear] = useState(false);
- const [sound, setSound] = useState();
-  
-  
-    async function playSound() {
-      const { sound } = await Audio.Sound.createAsync(
-        require('../assets/sounds/wrong.mp3')
-      )
-      setSound(sound);
-      await sound.playAsync();
-    }
+  const [finishTimer, setFinishTimer] = useState(false);
+  const [sound, setSound] = useState();
+  const [pressed, setPressed] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const [time, setTime] = useState(true);
+
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/sounds/wrong.mp3")
+    );
+    setSound(sound);
+    await sound.playAsync();
+  }
 
   useEffect(async () => {
     await axios({
@@ -52,24 +57,19 @@ export default function QuestionPage({ route, navigation }) {
       })
       .catch((error) => console.error(error));
   }, []);
-  const success = (x)  => {
-    if(x === 1)
-    return (
-       
-        <ContainedButton text={'hi'}/>
-     
-    )
-   
-    else{
-         playSound();
-        console.log('wrong')
+  const success = (x) => {
+    if (x === 1) {
+      setScore(score + 1);
+      console.log(score);
+    } else {
+      playSound();
+      
     }
-}
+  };
 
   if (cards) {
     let card = cards[currentQuestion];
     return (
-      
       <View style={styles.container}>
         <View style={styles.nav}>
           <TouchableOpacity
@@ -78,26 +78,45 @@ export default function QuestionPage({ route, navigation }) {
           >
             <AntDesign name="close" size={24} color="black" />
           </TouchableOpacity>
-          <MaterialCommunityIcons
+          {/* <MaterialCommunityIcons
             name="alarm-multiple"
             size={24}
             color="black"
+          /> */}
+          <CountDown
+            until={100}
+            timeToShow={["M",'S']}
+            digitStyle={{
+              backgroundColor: "#FFF",
+              borderWidth: 2,
+              borderRadius: 40,
+              borderColor: "#1CC625",
+            }}
+            onFinish={() => {
+              playSound();
+              // setAnswered(true);
+              // setFinishTimer(true);
+            }}
+            onPress={() => alert("hello")}
+            size={20}
           />
           <Logo />
         </View>
 
         {
-        // cards.map((card) => {
-         
+          // cards.map((card) => {
+
           <>
             <Image
               source={{ uri: IMAGE_URL + card.question.question_image }}
               style={styles.image}
             ></Image>
             <View>
-              <Text style={styles.question}>{card.question.question}</Text>
+              <Text style={styles.question}>
+                {card.question.question} {score}
+              </Text>
             </View>
-            <View style={styles.btnFlex}>
+            {/* <View style={styles.btnFlex}>
               {card.answers?.map((answer) => {
                 return (
                   <ContainedButton
@@ -106,9 +125,12 @@ export default function QuestionPage({ route, navigation }) {
                     textStyle={styles.text}
                     isCorrect={answer.is_correct}
                     onPress={() => {
+                      setAnswered(true);
                       success(answer.is_correct);
                       //
                     }}
+                    disabled={answered}
+                    onTime = {false}
                   />
                 );
               })}
@@ -118,15 +140,65 @@ export default function QuestionPage({ route, navigation }) {
                 buttonStyle={styles.btn}
                 text={"Next"}
                 onPress={() => {
-                  currentQuestion >= lengthh ? 
-                  navigation.navigate("Category", {category: category})
+                  setAnswered(false);
+           currentQuestion  === lengthh - 1 ? 
+                  score < lengthh ?
+                  navigation.navigate("Failed"):
+                  navigation.navigate("Congrats")
+
                   :setCurrentQuestion(currentQuestion + 1);
+                  console.log(score);
+                  console.log(currentQuestion);
+                   setFinishTimer(false);
+                  // setTime(!time);
+                }}
+              />
+            </View> */}
+            <View>
+              {card.answers?.map((answer) => {
+                return (
+                  <ContainedButton
+                    buttonStyle={
+                      pressed && answer.is_correct === 0
+                        ? styles.btnWrong
+                        : pressed && answer.is_correct === 1
+                        ? styles.btnCorrect
+                        : styles.btn
+                    }
+                    text={answer.name}
+                    textStyle={styles.text}
+                    isCorrect={answer.is_correct}
+                    onPress={() => {
+                      success(answer.is_correct);
+                      setPressed(true);
+                      // setFinishTimer(false);
+                    }}
+                    disabled={pressed}
+                  />
+                );
+              })}
+            </View>
+            <View>
+              <ContainedButton
+                buttonStyle={styles.btn}
+                text={"Next"}
+                onPress={() => {
+                  setPressed(false);
+                  currentQuestion  === lengthh - 1 ? 
+                  score < lengthh ?
+                  navigation.navigate("Failed"):
+                  navigation.navigate("Congrats")
+
+                  :setCurrentQuestion(currentQuestion + 1);
+                  console.log(score);
+
+                 
                 }}
               />
             </View>
           </>
           // ;
-        // })
+          // })
         }
       </View>
     );
@@ -138,67 +210,81 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     backgroundColor: "white",
   },
-  container:{
-    flex:1,
-},
-nav:{
-    flexDirection:'row',
-    justifyContent:'space-between',
-    padding:20,
-    marginTop:25,
-},
-image:{
-    height:'30%',
-    width:' 90%', 
-    borderRadius:30,
-    marginLeft:15,
-    marginRight:20
-},
-questionNum:{
-    color:'#BB271A',
-    fontWeight:'bold',
-    fontSize:18,
-    marginTop:20,
-    marginRight:20,
-    marginLeft:20,
-},
-question:{
-    fontSize:16,
-    marginRight:20,
-    marginTop:20,
-    marginLeft:20,
-},
-btnSuccess:{
-    backgroundColor:'green',
-    width:'90%',
-    marginLeft:20,
-    marginTop:20,
-    borderRadius:40
-},
-btn:{
-    backgroundColor:'#F29D38',
-    width:'90%',
-    marginLeft:20,
-    marginTop:20,
-    borderRadius:40
-},
-btnFail:{
-    backgroundColor:'red',
-    width:'90%',
-    marginLeft:20,
-    marginTop:20,
-    borderRadius:40
-},
-text:{
-    fontSize:35,
-    color:'#BB271A',
-    textAlign:'center',
-    padding:10
-},
-text2:{
-    fontSize:35,
-    color:'#778899',
-    textAlign:'center',
-    padding:10
-}
+  container: {
+    flex: 1,
+  },
+  nav: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+    marginTop: 25,
+  },
+  image: {
+    height: "30%",
+    width: " 90%",
+    borderRadius: 30,
+    marginLeft: 15,
+    marginRight: 20,
+  },
+  questionNum: {
+    color: "#BB271A",
+    fontWeight: "bold",
+    fontSize: 18,
+    marginTop: 20,
+    marginRight: 20,
+    marginLeft: 20,
+  },
+  question: {
+    fontSize: 16,
+    marginRight: 20,
+    marginTop: 20,
+    marginLeft: 20,
+  },
+  btnSuccess: {
+    backgroundColor: "green",
+    width: "90%",
+    marginLeft: 20,
+    marginTop: 20,
+    borderRadius: 40,
+  },
+  btn: {
+    backgroundColor: "#F29D38",
+    width: "90%",
+    marginLeft: 20,
+    marginTop: 20,
+    borderRadius: 40,
+  },
+  btnFail: {
+    backgroundColor: "red",
+    width: "90%",
+    marginLeft: 20,
+    marginTop: 20,
+    borderRadius: 40,
+  },
+  text: {
+    fontSize: 35,
+    color: "#BB271A",
+    textAlign: "center",
+    padding: 10,
+  },
+  text2: {
+    fontSize: 35,
+    color: "#778899",
+    textAlign: "center",
+    padding: 10,
+  },
+  btnWrong: {
+    backgroundColor: "red",
+    width: "90%",
+    marginLeft: 20,
+    marginTop: 20,
+    borderRadius: 40,
+  },
+  btnCorrect: {
+    backgroundColor: "green",
+    width: "90%",
+    marginLeft: 20,
+    marginTop: 20,
+    borderRadius: 40,
+  },
 });
