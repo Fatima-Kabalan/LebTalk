9c97cf6a2c5ae3b7a863a300814bf5bdba46115b
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-26 17:06:06 +0200

    admin-panel<>adding categoty page

--- admin-panel/src/Pages/CategoryPAge/CategoryPage.js
+++ admin-panel/src/Pages/CategoryPAge/CategoryPage.js
@@ -10,41 +10,18 @@ import CategoryStyle from  '../CategoryPAge/CategoryStyle.css';
 import AddPopup from '../../Components/AddPopup/AddPopup';
 import PopupStyle from "../../Components/AddPopup/PopupStyle.css";
 import Popup from 'reactjs-popup';
-import request from '../../config/axios'
+import { REACT_APP_BASE_URL } from "../../env.js";
+import axios from 'axios';
+import CategoryDialog from '../../Components/FormDialog/CategoryDialog';
 
 
 export default function CategoryPage() {
 
   const [open, setOpen] = React.useState(true);
-  const [name , setName] = React.useState("");
-  const [image , setImage] = React.useState("");
-
+  
   const handleDrawerOpen = () => {
     setOpen(true);
   };
-
-//   const handleDrawerClose = () => {
-//     setOpen(false);
-//   };
-
-    
-const save = async () => {
-request({
-  method: "post",
-  url: "addCategory",
-  data: {
-    category_name :` ${name}` , 
-    category_image : `${image}`, 
-    admin_id: 1 , 
-  },
-})
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-}
 
   return (
     <div>
@@ -61,39 +38,8 @@ request({
             </Box> 
         </div>
         <div>
-        <Popup trigger={<button className='btn'> Add Category </button>} 
-     position="top center">
-      <div className="form-popup" id="myForm">
-        <form action="/action_page.php" class="form-container">
-            <h1>Add Category</h1>
-
-            <label for="Category Name"><b>Category Name</b></label>
-            <input type="text" placeholder="Enter name" 
-            onChange = {(e) => {
-              setName(e.target.value);
-            }} />
-
-            <label for="text"><b>Category Image</b></label>
-            <input type="text" placeholder="Enter Image" 
-            onChange={(e) => {
-              setImage(e.target.value);
-            }}
-              />
-
-            <button type="submit" className="btn" 
-            onClick={
-              (e)=>{ 
-               e.preventDefault();
-                 console.log(name);
-                save();
-              }
-            
-            }>Save</button>
-            <button type="button" className="btn cancel" onclick="closeForm()">Close</button>
-        </form>
-      </div>
-    </Popup>
-            </div>
+            <CategoryDialog/>
+        </div>
     </div>
   );
 }
