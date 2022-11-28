0e1daf3474d0274df138c5a2029824b940bec867
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-29 01:57:26 +0200

    admin-panel<>fixingthe category page

--- admin-panel/src/Pages/CategoryPAge/CategoryPage.js
+++ admin-panel/src/Pages/CategoryPAge/CategoryPage.js
@@ -1,45 +1,37 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import NavBar from '../../Components/NavBar';
-import SideBar from '../../Components/SideBar';
-import Content from '../../Components/Content';
-import { Link } from 'react-router-dom';
-import CategoryTable from '../../Components/CategoryTable';
-import CategoryStyle from  '../CategoryPAge/CategoryStyle.css';
-import AddPopup from '../../Components/AddPopup/AddPopup';
-import PopupStyle from "../../Components/AddPopup/PopupStyle.css";
-import Popup from 'reactjs-popup';
-import { REACT_APP_BASE_URL } from "../../env.js";
-import axios from 'axios';
-import CategoryDialog from '../../Components/FormDialog/CategoryDialog';
-
+import * as React from "react";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import NavBar from "../../Components/NavBar";
+import SideBar from "../../Components/SideBar/SideBar";
+import Content from "../../Components/Content";
+import { Link } from "react-router-dom";
+import CategoryTable from "../CategoryPAge/CategoryTable";
+import CategoryDialog from "../CategoryPAge/CategoryDialog";
 
 export default function CategoryPage() {
-
   const [open, setOpen] = React.useState(true);
-  
+
   const handleDrawerOpen = () => {
     setOpen(true);
   };
 
   return (
     <div>
-        <div>
-            <Box sx={{ display: 'flex' }}>
-            <Link to ="/category">
+      <div>
+        <Box sx={{ display: "flex" }}>
+          <Link to="/category">
             <button>Hi</button>
-                <CssBaseline />
-                <NavBar open={open} handleDrawerOpen={handleDrawerOpen} />
-                <SideBar open={open} handleDrawerClose={handleDrawerOpen} />
-                <Content open={open} />
-            </Link>
-            <CategoryTable/>
-            </Box> 
-        </div>
-        <div>
-            <CategoryDialog/>
-        </div>
+            <CssBaseline />
+            <NavBar open={open} handleDrawerOpen={handleDrawerOpen} />
+            <SideBar open={open} handleDrawerClose={handleDrawerOpen} />
+            <Content open={open} />
+          </Link>
+          <CategoryTable />
+        </Box>
+      </div>
+      <div>
+        <CategoryDialog />
+      </div>
     </div>
   );
 }
