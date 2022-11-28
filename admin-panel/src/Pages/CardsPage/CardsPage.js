3273ac48c33b6df9f276226a8daa2aa63f7f344a
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-29 01:56:23 +0200

    admin-panel<>fixing cards page

--- admin-panel/src/Pages/CardsPage/CardsPage.js
+++ admin-panel/src/Pages/CardsPage/CardsPage.js
@@ -2,15 +2,11 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import NavBar from '../../Components/NavBar';
-import SideBar from '../../Components/SideBar';
+import SideBar from '../../Components/SideBar/SideBar';
 import Content from '../../Components/Content';
 import { Link } from 'react-router-dom';
-import CardTable from '../../Components/CardTable';
-import AddPopup from '../../Components/AddPopup/AddPopup';
-import PopupStyle from "../../Components/AddPopup/PopupStyle.css";
-import Popup from 'reactjs-popup';
-import CategoryStyle from  '../CategoryPAge/CategoryStyle.css';
-import CardDialog from '../../Components/FormDialog/CardDialog'
+import CardTable from './CardTable';
+import CardDialog from './CardDialog';
 
 export default function CardsPage() {
 
@@ -20,16 +16,11 @@ export default function CardsPage() {
     setOpen(true);
   };
 
-//   const handleDrawerClose = () => {
-//     setOpen(false);
-//   };
-
   return (
     <div>
         <div>
             <Box sx={{ display: 'flex' }}>
             <Link to ="/card">
-            <button>Hi</button>
                 <CssBaseline />
                 <NavBar open={open} handleDrawerOpen={handleDrawerOpen} />
                 <SideBar open={open} handleDrawerClose={handleDrawerOpen} />
@@ -40,36 +31,7 @@ export default function CardsPage() {
         </div>
         <div>
           <CardDialog/>
-        {/* <Popup trigger={<button className='btn'> Add Card </button>} 
-     position="center" >
-      <div className="form-popup" id="myForm">
-        <form action="/action_page.php" class="form-container">
-            <h1>Add Card</h1>
-
-            <label for="Card Name"><b>Card Name</b></label>
-            <input type="text" placeholder="Enter name" name="name" required />
-
-            <label for="text"><b>Card Image</b></label>
-            <input type="text" placeholder="Enter Image" name="image" required />
-
-            <label for="text"><b>Voice Note</b></label>
-            <input type="text" placeholder="Enter url" name="image" required />
-
-            <label for="text"><b>English Text</b></label>
-            <input type="text" placeholder="Enter Text" name="image" required />
-
-            <label for="text"><b>Arabic Text</b></label>
-            <input type="text" placeholder="Enter Text" name="image" required />
-
-            <label for="text"><b>Categories ID</b></label>
-            <input type="text" placeholder="Enter " name="image" required />
-
-            <button type="submit" className="btn">Save</button>
-            <button type="button" className="btn cancel" onclick="closeForm()">Close</button>
-        </form> */}
-      {/* </div>
-    </Popup> */}
-            </div>
+        </div>
     </div>
   );
 }
