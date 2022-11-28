0c78fc88fc611797c326e499e6c3494eb41a7384
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-29 01:54:45 +0200

    admin-panel<>removing unnecessary tags in topbar component

--- admin-panel/src/Components/TopBar/TopBar.js
+++ admin-panel/src/Components/TopBar/TopBar.js
@@ -5,10 +5,8 @@ import "./TopBar.css";
 const TopBar = () => {
   return (
     <div className="top-bar">
-      <div className="leb-text">
-      {/* <img src={logo} alt="logo" /> */}
-      <h1 className="leb-text">LebTalk</h1>
-      </div>
+      <img src={logo} alt="logo" />
+      <h1 className="leb-text">Leb<span className="talk-text">Talk</span></h1>
     </div>
   );
 };
