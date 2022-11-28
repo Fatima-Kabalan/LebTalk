3f889f315907b22bc70cc32df1a5ddcbc7bae4d5
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-11-29 01:50:31 +0200

    admin-panel<>fixing the navbar component

--- admin-panel/src/Components/NavBar.js
+++ admin-panel/src/Components/NavBar.js
@@ -3,8 +3,6 @@ import { styled } from '@mui/material/styles';
 import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 
 const drawerWidth = 240;
 
@@ -29,15 +27,6 @@ export default function NavBar({handleDrawerOpen, open}) {
   return (
       <AppBar position="fixed" open={open} sx={{backgroundColor : '#BD4543' }}> 
         <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{ mr: 2, ...(open && { display: 'none' }) }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" noWrap component="div">
             Admin Panel
           </Typography>
